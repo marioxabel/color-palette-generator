ae4f5660c5b40d86b1865882d69c2a045b4b2c00
marioxabel <31260739+marioxabel@users.noreply.github.com>
2023-06-14 13:42:13 -0600

    v.2.0

---
+++ README.md
@@ -0,0 +1,39 @@
+# 🎨 Color Palette Generator 
+
+## Description
+
+*Color Palette Generator  is a simple website that generates a simple scheme of colors based on an input, is fueled by [THE COLOR API](https://www.thecolorapi.com/).*
+
+The goal of this project was to build a color scheme generator using HTML, CSS, JavaScript and Git version control for module 9 of scrimba's The Frontend Developer Career Path.
+
+## Features
+
+When a user selects an input color and mode, they get a scheme based on the selected parameters. Also the user can click the names of the colors to copy them to the clipboard.
+
+
+## Technologies
+
+- HTML
+- CSS
+- JavaScript
+- Git
+
+## Author
+
+Mario Abel Garcia. Financial Engineer.
+
+## License
+
+**GNU General Public License v3.0** 
+
+*Permissions of this strong copyleft license are conditioned on making available 
+complete source code of licensed works and modifications, which include larger 
+works using a licensed work, under the same license. Copyright and license notices 
+must be preserved. Contributors provide an express grant of patent rights.*
+
+
+### Sources 
+
+- https://scrimba.com/learn/frontend
+
+
--- index.js
+++ index.js
@@ -1,27 +1,56 @@
 const numberOfColors = 6
+// CSS variable for displayin "n" number of columns
+document.documentElement.style.setProperty('--columns', numberOfColors);
 let colorScheme
+let colorPalette = ""
+let colorNames = ""
 
-document.querySelector("form").addEventListener("submit", e => {
-    e.preventDefault()
-    // get & clean parameters
+// Generate the color-palette and color-name divs
+for (i = 0; i < numberOfColors; i ++) {
+    colorPalette += `<div id="color-${i}" class="color"></div>`
+    colorNames += `<p id="color-${i}-name" data-color="color-name">wut</p>`
+}
+ 
+function fetchooor() {
+    // Get & clean parameters for API
     let color = document.getElementById("color-selector").value
     color = color.substring(1)
     const mode = document.getElementById("mode-selector").value
-    // fetch API
+    // Fetch API
     fetch(`https://www.thecolorapi.com/scheme?hex=${color}&format=json&mode=${mode}&count=${numberOfColors}`)
         .then(response => response.json())
         .then(data => {
             colorScheme = data.colors
             render()
         })    
-    console.log(color)
+}
+
+// Form button logic
+document.querySelector("form").addEventListener("submit", e => {
+    e.preventDefault()
+    fetchooor() 
+})
+
+// Click to copy using clipboard API
+document.body.addEventListener("click", e => {
+    if (e.target.attributes["data-color"]) {
+    let textToCopy= e.target.innerText
+    // clipboardAPI
+    navigator.clipboard.writeText(textToCopy)
+    }
 })
 
 function render() {
     colorScheme.map((color, index) => {
-        // set background color
+        // Set background color
         document.getElementById(`color-${index}`).style.backgroundColor = color.hex.value
-        // set names in hex
+        // Set names in hex
         document.getElementById(`color-${index}-name`).innerText = color.hex.value
     })
 }
+
+
+// Set the divs  and run the "start app" fetch
+document.getElementById("color-palette").innerHTML = colorPalette
+document.getElementById("color-names").innerHTML = colorNames
+fetchooor()
